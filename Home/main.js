75fea38fefe3f2db535c85ec72adb652720d52ba
kev-riv <kevin.rivrosado@gmail.com>
2023-12-01 09:18:35 -0400

    Commit html, css and js

---
+++ Home/main.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Simulate a loading time (you can remove this in production)
+    setTimeout(function() {
+        // Hide the loader after 2 seconds
+        document.querySelector("#loader").style.display = "none";
+        document.querySelector("#loader-wrapper-img").style.display = "none";
+        document.querySelector("#loader-wrapper").style.backgroundColor = "transparent";
+    }, 2000);
+    
+});
+
+
